b5d4727be729bb13cf7d6ee2db7ee20b54e5a6c5
crynobone <crynobone@gmail.com>
2012-05-31 20:38:48 +0800

    Rename Eloquent to Client
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ libraries/auth/client.php
@@ -0,0 +1,6 @@
+<?php namespace OneAuth\Auth;
+
+class Client extends \Eloquent
+{
+	public static $table = 'oneauth_clients';
+}
--- libraries/auth/model.php
+++
@@ -1,6 +0,0 @@
-<?php namespace OneAuth\Auth;
-
-class Model extends \Eloquent
-{
-	public static $table = 'oneauths';
-}
