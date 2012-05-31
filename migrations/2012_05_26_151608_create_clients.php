46e3d84e964f9bf80318b6e1bca7a5d3c190f3aa
crynobone <crynobone@gmail.com>
2012-05-31 20:37:54 +0800

    Readd migrations
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ migrations/2012_05_26_151608_create_clients.php
@@ -0,0 +1,37 @@
+<?php
+
+class OneAuth_Create_Clients {
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('oneauth_clients', function ($table)
+		{
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->string('provider', 50);
+			$table->string('uid', 255);
+			$table->string('access_token', 255)->nullable();
+			$table->string('refresh_token', 255)->nullable();
+			$table->integer('expires')->defaults(0)->nullable();
+
+			$table->timestamps();
+			$table->index('access_token');
+			$table->index('user_id');
+			$table->unique(array('provider', 'uid'));
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('oneauth_clients');
+	}
+}
