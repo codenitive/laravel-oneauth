d9c7be94abf27a668a40a2286c0a277e0a0efc54
crynobone <crynobone@gmail.com>
2012-07-25 13:07:52 +0800

    Allow Orchestra to configure OneAuth config

--- orchestra.php
+++ orchestra.php
@@ -17,6 +17,52 @@ Config::set('oneauth::urls', array(
 	'logged_in'    => handles('orchestra'),
 ));
 
+Orchestra\Extension\Config::map('oneauth', array(
+	'dropbox_key'       => 'oneauth::api.providers.dropbox.key',
+	'dropbox_secret'    => 'oneauth::api.providers.dropbox.secret',
+	
+	'facebook_id'       => 'oneauth::api.providers.facebook.id',
+	'facebook_secret'   => 'oneauth::api.providers.facebook.secret',
+	'facebook_scope'    => 'oneauth::api.providers.facebook.scope',
+	
+	'flickr_key'        => 'oneauth::api.providers.flickr.key',
+	'flickr_secret'     => 'oneauth::api.providers.flickr.secret',
+	
+	'foursquare_id'     => 'oneauth::api.providers.foursquare.id',
+	'foursquare_secret' => 'oneauth::api.providers.foursquare.secret',
+	
+	'github_id'         => 'oneauth::api.providers.github.id',
+	'github_secret'     => 'oneauth::api.providers.github.secret',
+	
+	'google_id'         => 'oneauth::api.providers.google.id',
+	'google_secret'     => 'oneauth::api.providers.google.secret',
+	
+	'instagram_id'      => 'oneauth::api.providers.instagram.id',
+	'instagram_secret'  => 'oneauth::api.providers.instagram.secret',
+	
+	'linkedin_key'      => 'oneauth::api.providers.linkedin.key',
+	'linkedin_secret'   => 'oneauth::api.providers.linkedin.secret',
+	
+	'paypal_id'         => 'oneauth::api.providers.paypal.id',
+	'paypal_secret'     => 'oneauth::api.providers.paypal.secret',
+	
+	'soundcloud_id'     => 'oneauth::api.providers.soundcloud.id',
+	'soundcloud_secret' => 'oneauth::api.providers.soundcloud.secret',
+	
+	'tumblr_key'        => 'oneauth::api.providers.tumblr.key',
+	'tumblr_secret'     => 'oneauth::api.providers.tumblr.secret',
+	
+	'twitter_key'       => 'oneauth::api.providers.twitter.key',
+	'twitter_secret'    => 'oneauth::api.providers.twitter.secret',
+	
+	'vimeo_key'         => 'oneauth::api.providers.vimeo.key',
+	'vimeo_secret'      => 'oneauth::api.providers.vimeo.secret',
+	
+	'windowlive_id'     => 'oneauth::api.providers.windowlive.id',
+	'windowlive_secret' => 'oneauth::api.providers.windowlive.secret',
+
+));
+
 /*
 |--------------------------------------------------------------------------
 | Integration with Orchestra
@@ -40,3 +86,5 @@ Event::listen('orchestra.logged.in', function()
 	$user = Auth::user();
 	Event::fire('oneauth.sync', array($user->id));
 });
+
+include_once Bundle::path('oneauth').'orchestra'.DS.'configure'.EXT;
---
+++ orchestra/configure.php
@@ -0,0 +1,101 @@
+<?php
+
+Event::listen('orchestra.form: extension.oneauth', function ($config, $form)
+{
+	$form->extend(function ($form)
+	{
+		$form->fieldset('Dropbox OAuth', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Dropbox Key', 'dropbox_key');
+			$fieldset->control('input:text', 'Dropbox Secret', 'dropbox_secret');
+		});
+
+		$form->fieldset('Facebook OAuth2', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Facebook ID', 'facebook_id');
+			$fieldset->control('input:text', 'Facebook Secret', 'facebook_secret');
+			$fieldset->control('input:text', 'facebook_scope', function ($control)
+			{
+				$control->label = 'Facebook Scope';
+				$control->value = function ($row)
+				{
+					$value = ! empty($row->facebook_scope) ? $row->facebook_scope : Config::get('oneauth::api.providers.facebook.scope');
+
+					return $value;
+				};
+			});
+		});
+
+		$form->fieldset('Flickr OAuth', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Flickr Key', 'flickr_key');
+			$fieldset->control('input:text', 'Flickr Secret', 'flickr_secret');
+		});
+
+		$form->fieldset('FourSquare OAuth2', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'FourSquare ID', 'foursquare_id');
+			$fieldset->control('input:text', 'FourSquare Secret', 'foursquare_secret');
+		});
+
+		$form->fieldset('Github OAuth2', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Github ID', 'github_id');
+			$fieldset->control('input:text', 'Github Secret', 'github_secret');
+		});
+
+		$form->fieldset('Google OAuth2', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Google ID', 'google_id');
+			$fieldset->control('input:text', 'Google Secret', 'google_secret');
+		});
+
+		$form->fieldset('Instagram OAuth2', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Instagram ID', 'instagram_id');
+			$fieldset->control('input:text', 'Instagram Secret', 'instagram_secret');
+		});
+
+		$form->fieldset('LinkedIn OAuth', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'LinkedIn Key', 'linkedin_key');
+			$fieldset->control('input:text', 'LinkedIn Secret', 'linkedin_secret');
+		});
+
+		$form->fieldset('Paypal OAuth2', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Paypal ID', 'paypal_id');
+			$fieldset->control('input:text', 'Paypal Secret', 'paypal_secret');
+		});
+
+		$form->fieldset('Soundcloud OAuth2', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Soundcloud ID', 'soundcloud_id');
+			$fieldset->control('input:text', 'Soundcloud Secret', 'soundcloud_secret');
+		});
+
+		$form->fieldset('Tumblr OAuth', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Tumblr Key', 'tumblr_key');
+			$fieldset->control('input:text', 'Tumblr Secret', 'tumblr_secret');
+		});
+
+		$form->fieldset('Twitter OAuth', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Twitter Key', 'twitter_key');
+			$fieldset->control('input:text', 'Twitter Secret', 'twitter_secret');
+		});
+
+		$form->fieldset('Vimeo OAuth', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'Vimeo Key', 'vimeo_key');
+			$fieldset->control('input:text', 'Vimeo Secret', 'vimeo_secret');
+		});
+
+		$form->fieldset('WindowLive OAuth2', function ($fieldset)
+		{
+			$fieldset->control('input:text', 'WindowLive ID', 'windowlive_id');
+			$fieldset->control('input:text', 'WindowLive Secret', 'windowlive_secret');
+		});
+	});
+});
