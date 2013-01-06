ab7ee85bfa8d20bdb0b9489153a4f778e35a7687
crynobone <crynobone@gmail.com>
2013-01-06 17:56:39 +0800

    [#31]Session::get('oneauth') does not clear after Auth::logout();
    Signed-off-by: crynobone <crynobone@gmail.com>

--- start.php
+++ start.php
@@ -44,6 +44,11 @@ Event::listen('oneauth.sync', function ($user_id)
 	return OneAuth\Auth\Core::sync($user_id);
 });
 
+Event::listen('laravel.auth: logout', function ()
+{
+	Session::forget('oneauth');
+});
+
 /*
 |--------------------------------------------------------------------------
 | OneAuth IoC
